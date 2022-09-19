5691c8b13734217988f172289907478518f4df87
FormulaXYZ <thomas.assefa6@gmail.com>
2022-09-19 10:56:40 -0700

    more foldes and fils are created

--- config/connection.js
+++ config/connection.js
@@ -1,2 +1,13 @@
+const sequelize = require('sequelize');
 const Sequelize = require('sequelize');
 require('dotenv').config();
+
+const sequelizeConnection = process.env.JAWSDB_URL
+? new sequelize(process.env.JAWSDB_URL)
+: new sequelize(process.env.DB_name,process.env.DB_USER,process.env.DB_PW,{
+    host:'localhost',
+    dialect:'mysql',
+    port: 3306
+});
+
+module.exports = sequelize;
